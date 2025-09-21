2ffcb4a804182551ed195fc9fc6af01cad29aa29
sacastilho <sabrinadcastilho@gmail.com>
2025-09-21 15:05:45 -0300

    adição de funcionalidade de busca pelo webservice e atividade agenda 07

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+using System.Threading.Tasks;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
@@ -9,16 +13,53 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+     
+
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                    if (t != null) 
+                    {
+                        string dados_previsao = "";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                            $"Longitude: {t.lon} \n" +
+                            $"Descrição: {t.description} \n" +
+                            $"Nacer do Sol: {t.sunrise} \n" +
+                            $"Por do Sol: {t.sunset} \n" +
+                            $"Temperatura máxima: {t.temp_max} \n" +
+                            $"Temperatura mínima: {t.temp_min} \n" +
+                            $"Valocidade do Vento: {t.speed} \n" +
+                            $"Visibilidade: {t.visibility} \n";
+                        
+                        lbl_res.Text = dados_previsao;
+
+                    } else
+                    {
+                        lbl_res.Text = "Cidade não encontrada";
+                    }
+
+                } else
+                {
+                    lbl_res.Text = "Preencha a cidade.";
+                }
+
+            }
+            catch (HttpRequestException)
+            {
+                await DisplayAlert("Erro", "Sem conexão com a internet.", "OK");
+            }
+
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+
         }
     }
 
