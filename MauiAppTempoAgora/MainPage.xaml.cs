b94961622d177ed0a88d1555455b1d1bdc31fae7
sacastilho <sabrinadcastilho@gmail.com>
2025-09-24 21:33:06 -0300

    localização agenda 08

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,13 +1,12 @@
 ﻿using MauiAppTempoAgora.Models;
 using MauiAppTempoAgora.Services;
+using System;
 using System.Threading.Tasks;
 
 namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
@@ -15,7 +14,7 @@ namespace MauiAppTempoAgora
 
      
 
-        private async void Button_Clicked(object sender, EventArgs e)
+        private async void Button_Clicked_Previsao(object sender, EventArgs e)
         {
             try
             {
@@ -39,6 +38,17 @@ namespace MauiAppTempoAgora
                         
                         lbl_res.Text = dados_previsao;
 
+                        string mapa = $"https://embed.windy.com/embed.html?" +
+                                      $"type=map&location=coordinates&metricRain=default&metricTemp=default" +
+                                      $"&metricWind=default&zoom=5&overlay=wind&product=ecmwf&level=surface" +
+                                      $"&lat={t.lat.ToString().Replace(",", ".")}&lon={t.lon.ToString().Replace(",", ".")}";
+
+                        wv_mapa.Source = mapa;
+
+
+                        Debug.WriteLine(mapa);
+
+
                     } else
                     {
                         lbl_res.Text = "Cidade não encontrada";
@@ -60,6 +70,66 @@ namespace MauiAppTempoAgora
                 await DisplayAlert("Ops", ex.Message, "OK");
             }
 
+        }
+
+        private async void Button_Clicked_localizacao(object sender, EventArgs e)
+        {
+            try
+            {
+                GeolocationRequest request = 
+                    new GeolocationRequest(
+                        GeolocationAccuracy.Medium,
+                        TimeSpan.FromSeconds(10)
+                    );
+
+                Location? local = await Geolocation.Default.GetLocationAsync(request);
+
+                if (local != null) 
+                {
+                    string local_disp = $"Latitude: {local.Latitude} \n" +
+                                        $"Longitude: {local.Longitude} \n";
+
+                    lbl_coords.Text = local_disp;
+
+                    // pega o nome da cidade que está nas cordenadas
+                    Getcidade(local.Latitude, local.Longitude);
+                }
+
+            } catch(FeatureNotSupportedException fnsEx)
+            {
+                await DisplayAlert("Erro: Dispositivo não suporta", fnsEx.Message, "OK");
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                await DisplayAlert("Erro: Localização Desabilitada", fneEx.Message, "OK");
+            }
+            catch (PermissionException pEx)
+            {
+                await DisplayAlert("Erro: Permissão de Localização", pEx.Message, "OK");
+            }
+            catch (Exception ex) 
+            {
+                await DisplayAlert("Errro", ex.Message, "OK");
+            }
+        }
+
+        private async void Getcidade(double lat, double lon)
+        {
+            try
+            {
+                IEnumerable<Placemark> places = await Geocoding.Default.GetPlacemarksAsync(lat, lon);
+
+                Placemark? place = places.FirstOrDefault();
+
+                if (place != null)
+                {
+                    txt_cidade.Text = place.Locality;
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro: Obtenção do nome da cidade", ex.Message, "OK");
+            }
         }
     }
 
